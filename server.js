bf4c58fb519d3691aa5ec009f40e8c7c0d0ab67a
eemr3 <eemr3@yahoo.com.br>
2020-04-14 16:03:14 -0300

    Update final do projeto DOE
    Integrando a aplicação com o banco de dados Postgres.

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ server.use(express.static('public'));
 //Habilitar body do formulário
 server.use(express.urlencoded({extended: true}));
 
+//Configurar a conexão com o banco de dados
+const Pool = require('pg').Pool;
+const db = new Pool({
+    user:'postgres',
+    password: 'damep9219',
+    host: 'localhost',
+    port: 5432,
+    database: 'doe'
+});
+
 //Configurando a template engine
 const nunjuks = require('nunjucks');
 nunjuks.configure('./', {
@@ -15,30 +25,14 @@ nunjuks.configure('./', {
     noCache: true
 })
 
-//Lista de doadores (Vetor ou Array)
-const donors=[
-    {
-        name: 'Emerson Moreira',
-        blood: 'AB+'
-    },
-    {
-        name: 'Rosane Moreira',
-        blood: 'B-'
-    },
-    {
-        name: 'Tiago Moreira',
-        blood: 'A+'
-    },
-    {
-        name: 'Daniel Moreira',
-        blood: 'O+'
-    }
-
-]
-
 //Configurar a apresentação da página
 server.get('/', (req, res) =>{
-   return res.render('index.html', {donors});
+    db.query("SELECT * FROM donors", (err, result) =>{
+        if(err) return res.send('Erro de banco de dados.');
+        
+        const donors = result.rows; 
+        return res.render('index.html', {donors});
+    })
 });
 
 server.post('/', (req, res) =>{
@@ -46,14 +40,24 @@ server.post('/', (req, res) =>{
     const name = req.body.name;
     const email = req.body.email;
     const blood = req.body.blood;
+    
+    if(name == "" || email == "" || blood == ""){
+        return res.send('Todos os campos são obrigatório');        
+    }
 
-    //Coloco valores dentro do Array
-    donors.push({
-        name: name,
-        blood: blood
-    })
+    //Coloco valores dentro do banco de dados
+    const query = `
+    INSERT INTO donors ("name", "email", "blood") VALUES ($1, $2, $3)`;
+    const values = [name, email, blood];
 
-    res.redirect('/');
+    db.query(query, values, (err) =>{
+        if(err){
+            return res.send('erro no banco de dados');
+        }
+        
+        return res.redirect('/');
+    });
+
 });
 
 //Aqui esta iniciando o servidor e permitindo o acesso na porta 3000
